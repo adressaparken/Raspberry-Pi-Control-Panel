35af9e123ea7e9892bdf0d2d103148bc050f2c0d
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-30 20:39:20 +0100

    .

--- index.js
+++ index.js
@@ -32,314 +32,51 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ex: res.render('users.html').
 app.set('view engine', 'html');
 
-// Dummy users
-var rpis = [
-    {
-        id: 1,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 2,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 3,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 4,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 5,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 6,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 7,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 8,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    },
-    {
-        id: 9,
-        status: false,
-        last_hearbeat: 0,
-        osc_port: 5005,
-        temperature_value: 0,
-        temperature_interval: 5,
-        temperature_mqtt: true,
-        temperature_osc: false,
-        pressure_value: 0,
-        pressure_interval: 5,
-        pressure_mqtt: true,
-        pressure_osc: false,
-        light_value: 0,
-        light_interval: 5,
-        light_mqtt: true,
-        light_osc: false,
-        pedestrians_value: 0,
-        pedestrians_interval: 5,
-        pedestrians_mqtt: true,
-        pedestrians_osc: false,
-        decibel_value: 0,
-        decibel_interval: 5,
-        decibel_mqtt: true,
-        decibel_osc: false
-    }
-];
+function RPi(id) {
+    this.id = id;
+    this.status = false;
+    this.last_hearbeat = 0;
+    this.osc_port = 5005;
+    this.temperature_value = 0;
+    this.temperature_interval = 5;
+    this.temperature_mqtt = true;
+    this.temperature_osc = false;
+    this.pressure_value = 0;
+    this.pressure_interval = 5;
+    this.pressure_mqtt = true;
+    this.pressure_osc = false;
+    this.light_value = 0;
+    this.light_interval = 5;
+    this.light_mqtt = true;
+    this.light_osc = false;
+    this.pedestrians_value = 0;
+    this.pedestrians_interval = 5;
+    this.pedestrians_mqtt = true;
+    this.pedestrians_osc = false;
+}
+
+var rpis = [];
+var i;
+for (i = 1; i < 10; i++) {
+    rpis.push( new RPi(i) );
+}
 
 app.get('/', function(req, res){
     res.render('index', {
         rpis: rpis,
         title: "Raspberry Pi Control Panel",
-        header: "Raspberry Pi Control Panel",
-        collapse: parseInt(req.query.coll)
+        header: "Raspberry Pi Control Panel"
     });
 });
 
-app.post('/change_global_settings', function(req, res, next){
-    // req.body object has your form values
 
-    var osc_port = req.body.oscPort
 
-    var temperature_interval = req.body.temperatureInterval
-    var temperature_mqtt = (req.body.temperatureMQTT != undefined ? 1 : 0);
-    var temperature_osc = (req.body.temperatureOSC != undefined ? 1 : 0);
-
-    var pressure_interval = req.body.pressureInterval
-    var pressure_mqtt = (req.body.pressureMQTT != undefined ? 1 : 0);
-    var pressure_osc = (req.body.pressureOSC != undefined ? 1 : 0);
-
-    var light_interval = req.body.lightInterval
-    var light_mqtt = (req.body.lightMQTT != undefined ? 1 : 0);
-    var light_osc = (req.body.lightOSC != undefined ? 1 : 0);
-
-    var pedestrians_interval = req.body.pedestriansInterval
-    var pedestrians_mqtt = (req.body.pedestriansMQTT != undefined ? 1 : 0);
-    var pedestrians_osc = (req.body.pedestriansOSC != undefined ? 1 : 0);
-
-    // var decibel_mqtt = (req.body.decibelMQTT != undefined ? 1 : 0);
-    // var decibel_osc = (req.body.decibelOSC != undefined ? 1 : 0);
-
-    var s = osc_port + ','
-
-    s += temperature_interval + ','
-    s += temperature_mqtt + ','
-    s += temperature_osc + ','
-
-    s += pressure_interval + ','
-    s += pressure_mqtt + ','
-    s += pressure_osc + ','
-
-    s += light_interval + ','
-    s += light_mqtt + ','
-    s += light_osc + ','
-
-    s += pedestrians_interval + ','
-    s += pedestrians_mqtt + ','
-    s += pedestrians_osc
-
-    // s += decibel_mqtt + ','
-    // s += decibel_osc
-
-    for (i = 1; i < 10; i++) {
-        client.publish('parken/rpi/' + i + '/settings', s)
-    }
-
-    setTimeout(function() {
-        //res.redirect(req.get('referer'));
-        res.redirect('/?coll=0');
-    }, 2000);
-
-});
-
-app.post('/change_settings', function(req, res, next){
-    // req.body object has your form values
+app.post('/', function(req, res){
 
     var id = parseInt(req.body.id);
 
+    console.log( id.toString() )
+
     var osc_port = req.body.oscPort
 
     var temperature_interval = req.body.temperatureInterval
@@ -357,9 +94,6 @@ app.post('/change_settings', function(req, res, next){
     var pedestrians_interval = req.body.pedestriansInterval
     var pedestrians_mqtt = (req.body.pedestriansMQTT != undefined ? 1 : 0);
     var pedestrians_osc = (req.body.pedestriansOSC != undefined ? 1 : 0);
-
-    // var decibel_mqtt = (req.body.decibelMQTT != undefined ? 1 : 0);
-    // var decibel_osc = (req.body.decibelOSC != undefined ? 1 : 0);
 
     var s = osc_port + ','
 
@@ -379,16 +113,19 @@ app.post('/change_settings', function(req, res, next){
     s += pedestrians_mqtt + ','
     s += pedestrians_osc
 
-    // s += decibel_mqtt + ','
-    // s += decibel_osc
-
-    client.publish('parken/rpi/' + id + '/settings', s)
+    if ( id == 0) {
+        for (i = 1; i < 10; i++) {
+            client.publish('parken/rpi/' + i + '/settings', s)
+        }
+    } else {
+        client.publish('parken/rpi/' + id + '/settings', s)
+    }
 
     setTimeout(function() {
-        //res.redirect(req.get('referer'));
-        res.redirect('/?coll=' + id);
+        res.redirect(req.get('referer'));
     }, 2000);
 });
+
 
 // get unit timestamp
 function get_time() {
@@ -398,7 +135,7 @@ function get_time() {
 var myInt = setInterval(function () {
     rpis.forEach(function(rpi) {
         if (get_time() >= rpi.last_hearbeat + 300) {
-            rpi.status = false
+            // rpi.status = false                                               // TODO CHANGE BACK!!!!!!!!!!!
         }
     });
 }, 20000);
@@ -412,7 +149,6 @@ client.on('connect', function () {
     client.subscribe('parken/rpi/+/pressure')
     client.subscribe('parken/rpi/+/light')
     client.subscribe('parken/rpi/+/pedestrians')
-    client.subscribe('parken/rpi/+/decibel')
 });
 
 client.on('message', function (topic, message) {
@@ -422,7 +158,6 @@ client.on('message', function (topic, message) {
     var pressure = new RegExp("parken/rpi/[0-9]+/pressure");
     var light = new RegExp("parken/rpi/[0-9]+/light");
     var pedestrians = new RegExp("parken/rpi/[0-9]+/pedestrians");
-    var decibel = new RegExp("parken/rpi/[0-9]+/decibel");
 
     var id = parseInt( topic.toString().split('/')[2] );
 
@@ -469,10 +204,6 @@ client.on('message', function (topic, message) {
     } else if ( pedestrians.test(topic.toString()) ) {
 
         rpis[id - 1].pedestrians_value = parseInt(message.toString());
-
-    } else if ( decibel.test(topic.toString()) ) {
-
-        rpis[id - 1].decibel_value = parseInt(message.toString());
 
     }
 });
