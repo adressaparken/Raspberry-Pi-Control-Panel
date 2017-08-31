53024d0f33fb93b93af5ef11f7659bd8f265c770
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-31 05:41:23 +0100

    .

--- scripts/start_native.sh
+++ scripts/start_native.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NATIVE_APP='index.js'
+NATIVE_APP='index'
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -11,7 +11,7 @@ while true; do
     # echo "Checking for updates"
     # sh $SCRIPT_DIR/pull_git_native.sh
     echo "Restarting native app!"
-    node $ROOT_DIR/$NATIVE_APP.py
+    node $ROOT_DIR/$NATIVE_APP.js
     echo "Main app ended..."
     echo "-----------------------------------"
     read -t 5 -p "Restarting the native app in 5 seconds. Press <CTRL>+c to stop."
